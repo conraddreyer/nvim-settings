0bbefaeda05d6e22c2f150b28daeeac62181be91
Conrad Dreyer <git@conraddreyer.com>
2023-11-15 13:00:03 +0100

    added table plugin

--- lua/don/plugins-setup.lua
+++ lua/don/plugins-setup.lua
@@ -104,6 +104,7 @@ return packer.startup(function(use)
 	-- formatting & linting
 	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
 	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+	use("dhruvasagar/vim-table-mode") -- format tables
 
 	-- treesitter configuration
 	use({
